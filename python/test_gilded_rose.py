44cb931ccb00c50ddba8cd5485d4175e8824c428
nirattisai.t <nirattisai.t@wisesight.com>
2022-06-28 10:31:29 +0700

    Change test gilded rose to nosetests

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,16 +1,44 @@
-# -*- coding: utf-8 -*-
-import unittest
-
-from gilded_rose import Item, GildedRose
+from gilded_rose import GildedRose, Item
+from nose.tools import assert_equal
 
 
-class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+class GildedRoseTest:
+    def __init__(self) -> None:
+        self.items = [
+            Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
+            Item(name="Aged Brie", sell_in=2, quality=0),
+            Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
+            Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+            Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+            Item(
+                name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20
+            ),
+            Item(
+                name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49
+            ),
+            Item(
+                name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49
+            ),
+            Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
+        ]
 
-        
-if __name__ == '__main__':
-    unittest.main()
+    def test_one_day_passed(self):
+        expected_result = [
+            Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
+            Item(name="Aged Brie", sell_in=1, quality=1),
+            Item(name="Elixir of the Mongoose", sell_in=4, quality=6),
+            Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=78),
+            Item(name="Sulfuras, Hand of Ragnaros", sell_in=-2, quality=78),
+            Item(
+                name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=21
+            ),
+            Item(
+                name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=50
+            ),
+            Item(
+                name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=50
+            ),
+            Item(name="Conjured Mana Cake", sell_in=2, quality=5),  # <-- :O
+        ]
+        GildedRose(self.items).update_quality()
+        assert_equal(expected_result, self.items)
