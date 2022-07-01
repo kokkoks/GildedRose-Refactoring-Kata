3504dbfa8fed2f567b9b73d8436e0669d0197de0
takipipo <ktphap.ncu@gmail.com>
2022-07-01 10:34:48 +0700

    Seperate update_quality 'if' for backstage and aged_bried

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -2,12 +2,14 @@
 AGED_BRIED = "Aged Brie"
 BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
 SULFURAS = "Sulfuras, Hand of Ragnaros"
+MAX_QUALITY = 50
 
 
 class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
+    # TODO: seperate update_quality & update_sell_in method
     def update_quality(self):
         for item in self.items:
             if (
@@ -16,15 +18,15 @@ class GildedRose(object):
                 and item.quality > 0
             ):
                 item.quality = item.quality - 1
-            else:
-                # Item is aged_bried or backstage_passes
-                if item.quality < 50:
+            elif item.name == BACKSTAGE_PASSES and item.quality < MAX_QUALITY:
+                if item.sell_in < 6:
+                    item.quality = min(item.quality + 3, MAX_QUALITY)
+                elif item.sell_in < 11:
+                    item.quality = min(item.quality + 2, MAX_QUALITY)
+                else:
                     item.quality = item.quality + 1
-                    if item.name == BACKSTAGE_PASSES:
-                        if item.sell_in < 11 and item.quality < 50:
-                            item.quality = item.quality + 1
-                        if item.sell_in < 6 and item.quality < 50:
-                            item.quality = item.quality + 1
+            elif item.name == AGED_BRIED and item.quality < MAX_QUALITY:
+                item.quality = item.quality + 1
 
             if item.name != BACKSTAGE_PASSES:
                 item.sell_in = item.sell_in - 1
@@ -36,7 +38,7 @@ class GildedRose(object):
                     else:
                         item.quality = 0
                 else:
-                    if item.quality < 50:
+                    if item.quality < MAX_QUALITY:
                         item.quality = item.quality + 1
 
 
